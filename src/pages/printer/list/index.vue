b524d31fd5d5f5b3ad4a33aa4c869c8a1d442839
lizilong <35825874+liyijieac@users.noreply.github.com>
2019-12-13 14:00:11 +0800

    add printer/list page
    添加打印机列表页面

---
+++ src/pages/printer/list/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="list">
+    <div class="p-list">
+        <div class="info-btn">
+          <div class="num">
+            <span>550254215</span>
+            <van-tag plain>前台</van-tag>
+          </div>
+          <div class="binding">解绑</div>
+        </div>
+        <div class="message">在线，工作状态正常</div>
+        <van-divider />
+    </div>
+    <div class="p-list">
+        <div class="info-btn">
+          <div class="num">
+            <span>550254215</span>
+            <van-tag plain>前台</van-tag>
+          </div>
+          <div class="binding">解绑</div>
+        </div>
+        <div class="message1">在线，工作状态不正常</div>
+        <van-divider />
+    </div>
+    <div class="p-list">
+        <div class="info-btn">
+          <div class="num">
+            <span>550254215</span>
+            <van-tag plain>前台</van-tag>
+          </div>
+          <div class="binding">解绑</div>
+        </div>
+        <div class="message2">离线</div>
+        <van-divider />
+    </div>
+    <div style="padding:15px 20px" @click="$router.push('/develop/edit')">
+      <van-button type="default" block>添加打印机</van-button>
+    </div>
+  </div>
+</template>
+<script src="./index.js"></script>
+<style lang="scss" scoped>
+@import "index.scss";
+</style>
